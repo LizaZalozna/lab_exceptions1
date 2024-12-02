b5ff6298738c47c0744260fa33ca3ecc527920cf
lizazalozna <liza.zalozna@gmail.com>
2024-12-02 13:40:13 +0200

    MainWithTry

--- lab_exceptions1/Program.cs
+++ lab_exceptions1/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
 
+namespace lab_exceptions1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int result = 0;
+            int count = 0;
+            StringBuilder no_file = new StringBuilder();
+            StringBuilder no_data = new StringBuilder();
+            StringBuilder overflow = new StringBuilder();
+            for (int i = 10; i < 30; i++)
+            {
+                try
+                {
+                    string[] input = File.ReadAllLines($"{i}.txt");
+                    int a = int.Parse(input[0]);
+                    int b = int.Parse(input[1]);
+                    checked
+                    {
+                        result += a * b;
+                    }
+                    count++;
+                }
+                
+            }
+        }
+    }
+}
